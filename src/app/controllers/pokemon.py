3fd1da492ffe82872d3da838ad4eb7d3d17d0201
Marcelo Coelho <92119579+MCoelho222@users.noreply.github.com>
2022-11-02 17:11:56 -0300

    ENDPOINT/GET/more victories over pokemon 7

--- src/app/controllers/pokemon.py
+++ src/app/controllers/pokemon.py
@@ -34,9 +34,80 @@ def list_all_generation_one_5x_winner_least():
     }, {
         '$limit': 10
     }
-])
+    ])
     return Response(
         response=json_util.dumps({"records": pokemons}),
         status=200,
         mimetype='application/json'
     )
+
+@pokemons.route("/more_victories_over_7", methods=['GET'])
+def list_more_vitories_over_seven():
+    winners = mongo_client.pokemons.aggregate([
+    {
+        '$lookup': {
+            'from': 'combats', 
+            'as': 'Winners', 
+            'let': {
+                'id_pokemon': '$#'
+            }, 
+            'pipeline': [
+                {
+                    '$match': {
+                        '$expr': {
+                            '$and': [
+                                {
+                                    '$eq': [
+                                        '$Winner', '$$id_pokemon'
+                                    ]
+                                }, {
+                                    '$or': [
+                                        {
+                                            '$eq': [
+                                                '$First_pokemon', 7
+                                            ]
+                                        }, {
+                                            '$eq': [
+                                                '$Second_pokemon', 7
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    }
+                }
+            ]
+        }
+    }, {
+        '$match': {
+            'Winners.0': {
+                '$exists': True
+            }, 
+            '#': {
+                '$ne': 7
+            }
+        }
+    }, {
+        '$project': {
+            '_id': 0,
+            '#': 1, 
+            'Name': 1, 
+            'Victories': {
+                '$size': '$Winners'
+            }
+        }
+    }, {
+        '$sort': {
+            'Victories': -1
+        }
+    }, {
+        '$limit': 10
+    }
+    ])
+    return Response(
+        response=json_util.dumps({"records": winners}),
+        status=200,
+        mimetype='application/json'
+    )
+
