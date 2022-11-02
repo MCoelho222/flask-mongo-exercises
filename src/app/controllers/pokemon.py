dae1a90dcd233947ab82f376f58fe3c92f4a7caf
Marcelo Coelho <92119579+MCoelho222@users.noreply.github.com>
2022-11-02 15:11:25 -0300

    ENDPOINT/GET/list pokemons of generation one with 5 victories at least

--- src/app/controllers/pokemon.py
+++ src/app/controllers/pokemon.py
@@ -6,12 +6,35 @@ from bson import json_util
 pokemons = Blueprint("pokemons", __name__, url_prefix="/pokemons")
 
 @pokemons.route("/list_all_generation_one", methods=["GET"])
-def list_all_generation_one():
-    pokemons = mongo_client.pokemons.aggregate([{
-        "$match": {
-            "generation": "1"
+def list_all_generation_one_5x_winner_least():
+    pokemons = mongo_client.pokemons.aggregate([
+    {
+        '$match': {
+            'Generation': 1
         }
-    }])
+    }, {
+        '$lookup': {
+            'from': 'combats', 
+            'localField': '#', 
+            'foreignField': 'Winner', 
+            'as': 'Winners'
+        }
+    }, {
+        '$match': {
+            'Winners.5': {
+                '$exists': True
+            }
+        }
+    }, {
+        '$project': {
+            '_id': 1, 
+            'Name': 1, 
+            'Generation': 1
+        }
+    }, {
+        '$limit': 10
+    }
+])
     return Response(
         response=json_util.dumps({"records": pokemons}),
         status=200,
